2c9dd262abb4816b32c8f8cd0981e44650ec428b
Lucas1988 <vergeest@gmail.com>
2018-11-23 00:41:18 +0100

    script added

---
+++ DebugTool.py
@@ -0,0 +1,21 @@
+import requests
+import json
+import re
+
+try:
+	print('Hello world!') + 3
+except Exception as title:
+	print(title)
+
+	result = requests.get('https://api.stackexchange.com/2.2/search/advanced?page=1&pagesize=1&order=desc&sort=activity&tagged=python&site=stackoverflow&title=' + str(title))
+
+	searchresult = result.json()
+	questionID = searchresult['items'][0]['question_id']
+
+	question = requests.get('https://api.stackexchange.com/2.2/questions/' + str(questionID) + '/answers?page=1&pagesize=1&order=desc&sort=votes&filter=withbody&site=stackoverflow')
+	question = question.json()
+
+	answer = question['items'][0]['body']
+	answer = re.sub('<.*?>', '', answer)
+
+	print(answer)
