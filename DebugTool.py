0d63e30d62a620663dbea47d6c47826309f5110d
Lucas1988 <vergeest@gmail.com>
2018-11-23 02:11:20 +0100

    small fixes

--- DebugTool.py
+++ DebugTool.py
@@ -4,11 +4,12 @@ import re
 import traceback
 
 try:
-	print('Hello') + 1
+	print('Hello') + 8 # This is an example
+
 except Exception as title:
 	print(traceback.format_exc())
 
-	result = requests.get('https://api.stackexchange.com/2.2/search/advanced?page=1&pagesize=1&order=desc&sort=relevance&tagged=python&site=stackoverflow&title="' + str(title) + '"')
+	result = requests.get('https://api.stackexchange.com/2.2/search/advanced?page=1&pagesize=1&order=desc&sort=relevance&tagged=python&site=stackoverflow&title=' + str(title))
 
 	searchresult = result.json()
 	questionID = searchresult['items'][0]['question_id']
