13fb1b83a862ce538a1d52b15818cb117d02f62b
Lucas1988 <vergeest@gmail.com>
2018-11-23 02:20:38 +0100

    small fix

--- DebugTool.py
+++ DebugTool.py
@@ -4,7 +4,7 @@ import re
 import traceback
 
 try:
-	print('Hello') + 8 # This is an example
+	print('Hello') + 8 # This is an example. Put your own code here.
 
 except Exception as title:
 	print(traceback.format_exc())
@@ -12,7 +12,11 @@ except Exception as title:
 	result = requests.get('https://api.stackexchange.com/2.2/search/advanced?page=1&pagesize=1&order=desc&sort=relevance&tagged=python&site=stackoverflow&title=' + str(title))
 
 	searchresult = result.json()
-	questionID = searchresult['items'][0]['question_id']
+	try:
+		questionID = searchresult['items'][0]['question_id']
+	except:
+		print('Unfortunately, it seems like no solution to your issue is available on StackOverflow.')
+		quit()
 
 	question = requests.get('https://api.stackexchange.com/2.2/questions/' + str(questionID) + '/answers?page=1&pagesize=1&order=desc&sort=votes&filter=withbody&site=stackoverflow')
 	question = question.json()
